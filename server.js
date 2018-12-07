248c0306e03c748fe27f33b931769750cb78e0a9
Oleg Proskurin <regx@usul.su>
2018-12-07 21:32:15 +0300

    deploy to heroku

---
+++ server.js
@@ -0,0 +1,35 @@
+const Koa = require('koa');
+
+
+const app = new Koa();
+
+
+const htmlTemplate = url => `<!DOCTYPE html>
+<html>
+    <head>
+        <title>Universal React server bundle</title>
+    </head>
+    <body>
+        <h1>Server Side</h1>
+        <div id="app">${(url)}</div>
+    </body>
+</html>`;
+
+app.use(async ctx => {
+  ctx.body = htmlTemplate(ctx.request.url);
+  console.log('method:', ctx.request.method);
+  console.log('url:', ctx.request.url);
+});
+
+app.on('error', err => {
+  console.log(err);
+});
+
+const port = process.env.PORT;
+
+if (!port) {
+  console.log(`Warning! process.env.PORT is ${port}. Looks like you started via "npm start". Please use "heroku local web" instead!`);
+  return
+}
+
+app.listen(port, () => console.log(`\nServer starts on ${port}`));
